ca09c53f39fc28400678b003a5eb8df2d8f4d4db
simo25541 <almotazakilmohammad2002@gmail.com>
2022-02-18 19:18:48 +0100

    the first page with firebase login

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  // extend: {},
+  theme: {
+    extend: {
+      colors: {
+        'default-bg': '#18191A',
+        'primary': '#9B28EB',
+        'secondary': '#F2BF00'
+      }
+    }
+  },
+  plugins: [],
+}
