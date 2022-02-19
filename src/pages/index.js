c35457b02ef21096fb91850fb5cac795de4cd720
simo25541 <almotazakilmohammad2002@gmail.com>
2022-02-19 09:52:34 +0100

    adding some animations the the index screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Index = () => {
-    return (<div className="grid items-center text-center content-center h-screen z-40 relative">
-        <div className="p-4 w-screen lg:w-1/2 m-auto">
-            <h2 className="text-5xl text-white font-bold mb-5">Welcome to <span className="text-secondary">Questions Race</span></h2>
-            <p className="text-white">The discription should be here Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                numquam blanditiis harum quisquam eius sed odit fugiat iusto fu</p>
-            <button className="bg-primary hover:bg-secondary-700 text-white font-bold py-2 px-4 rounded-full mt-5 cursor-pointer">Creat A New Private Game</button>
+    const animations = {
+        'show': {
+            y: 0,
+            opacity: 1
+        },
+        'hidden': {
+            y: '-10vw',
+            opacity: 0,
+        },
+        'transition': { type: 'tween', duration: 0.8, ease: [0.515, 0.015, 0.535, 0.985] }
+    };
+    return (<React.Fragment>
+        <div className="grid items-center text-center content-center h-screen z-40 relative">
+            <div className="p-4 w-screen lg:w-1/2 m-auto">
+                <motion.h2 initial={animations.hidden} transition={{ ...animations.transition, ...{ 'delay': 0.5 } }} animate={animations.show} className="text-5xl text-white font-bold mb-5">Welcome to <span className="text-secondary">Questions Race</span></motion.h2>
+                <motion.p initial={animations.hidden} transition={{ ...animations.transition, ...{ 'delay': 0.2 } }} animate={animations.show} className="text-white">The discription should be here Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fu</motion.p>
+                <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }} className="bg-primary hover:bg-secondary-700 text-white font-bold py-2 px-4 rounded-full mt-5 cursor-pointer">Creat A New Private Game</motion.button>
+            </div>
         </div>
-    </div>)
+    </React.Fragment>)
 };
 
 export default Index;
